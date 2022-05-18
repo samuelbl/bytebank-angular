a0ad2ae11531f693f5b6ccb3b6f226d2c711d431
Samuel Loli <bristot@gmail.com>
2022-05-18 10:48:27 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'bytebank';
+  transferencias: any[] = [];
+
+  transferir($event) {
+    const transferencia = {...$event, data: new Date()}
+    this.transferencias.push(transferencia);
+  }
+}
